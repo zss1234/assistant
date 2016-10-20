b3dbf06f241addbd0d064714d1d6ac55115f5a08
zhaoshasha <zss_27149@aliyun.com>
2016-10-20 10:00:33 +0800

    getUserInfo.php

---
+++ getUserInfo.php
@@ -0,0 +1,29 @@
+<?php 
+$appid = "wxdf937732890770c1";  
+$secret = "d4624c36b6795d1d99dcf0547af5443d";  
+$code = $_GET["code"];
+ 
+//第一步:取得openid
+$oauth2Url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$secret&code=$code&grant_type=authorization_code";
+$oauth2 = getJson($oauth2Url);
+  
+//第二步:根据全局access_token和openid查询用户信息  
+$access_token = $oauth2["access_token"];  
+$openid = $oauth2['openid'];  
+$get_user_info_url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=$access_token&openid=$openid&lang=zh_CN";
+$userinfo = getJson($get_user_info_url);
+ 
+//打印用户信息
+  print_r($userinfo);
+ 
+function getJson($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return json_decode($output, true);
+}
+?>
