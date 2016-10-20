4ec80d2e03666d7b487f3c55a119a79d46c99ca6
zhaoshasha <zss_27149@aliyun.com>
2016-10-20 10:55:48 +0800

    update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 //scope=snsapi_userinfo实例
 $appid='wxdf937732890770c1';
-$redirect_uri = urlencode ( 'http://1.zss27149.applinzi.com');
+$redirect_uri = urlencode ( 'http://1.zss27149.applinzi.com/getUserInfo.php' );
 $url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
 header("Location:".$url);
 ?>
