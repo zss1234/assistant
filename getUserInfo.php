072116098b81c2c67a4cd940de819b4bfa5ca235
zhaoshasha <zss_27149@aliyun.com>
2016-10-21 11:36:05 +0800

    reset

--- getUserInfo.php
+++ getUserInfo.php
@@ -1,36 +1,72 @@
 <?php 
-$appid = "wxdf937732890770c1";  
-$secret = "d4624c36b6795d1d99dcf0547af5443d";  
-$code = $_GET["code"];
-//第一步:取得openid
-$oauth2Url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".$appid."&secret=".$secret."&code=".$code."&grant_type=authorization_code";
-// $oauth2 = getJson($oauth2Url);
-//第二步:根据全局access_token和openid查询用户信息  
-// $access_token = $oauth2["access_token"];
-// $openid = $oauth2['openid']; 
-$token = json_decode(file_get_contents($oauth2Url));
+// $appid = "wxdf937732890770c1";  
+// $secret = "d4624c36b6795d1d99dcf0547af5443d";  
+// $code = $_GET["code"];
+// //第一步:取得openid
+// $oauth2Url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".$appid."&secret=".$secret."&code=".$code."&grant_type=authorization_code";
+// // $oauth2 = getJson($oauth2Url);
+// //第二步:根据全局access_token和openid查询用户信息  
+// // $access_token = $oauth2["access_token"];
+// // $openid = $oauth2['openid']; 
+// $token = json_decode(file_get_contents($oauth2Url));
 
-$access_token = $token->access_token;
-$openid		  = $token->openid;
+// $access_token = $token->access_token;
+// $openid		  = $token->openid;
 
-echo "aa".$access_token."<br>".$openid;	
+// echo "aa".$access_token."<br>".$openid;	
 
-// $access_token = "9F_K9CcZU35ICCJT0NsnaP3t9g03tjOIRNGqeJjWw-6Ikr_rsDGZ8mxWxRDGSFh6rVDXFq1jXnfRDqV3PqtB8oM1XN7vY1EBuIDvbS0isr7sgU2K7sreM_N8BnO3IbE6EGGaABAKMM";
-// $openid = "";
-// $get_user_info_url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=$access_token&openid=$openid&lang=zh_CN";
-// $userinfo = getJson($get_user_info_url);
+// // $access_token = "9F_K9CcZU35ICCJT0NsnaP3t9g03tjOIRNGqeJjWw-6Ikr_rsDGZ8mxWxRDGSFh6rVDXFq1jXnfRDqV3PqtB8oM1XN7vY1EBuIDvbS0isr7sgU2K7sreM_N8BnO3IbE6EGGaABAKMM";
+// // $openid = "";
+// // $get_user_info_url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=$access_token&openid=$openid&lang=zh_CN";
+// // $userinfo = getJson($get_user_info_url);
  
-//打印用户信息
-  // print_r($userinfo);
+// //打印用户信息
+//   // print_r($userinfo);
  
-function getJson($url){
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); 
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $output = curl_exec($ch);
-    curl_close($ch);
-    return json_decode($output, true);
-}
+// function getJson($url){
+//     $ch = curl_init();
+//     curl_setopt($ch, CURLOPT_URL, $url);
+//     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
+//     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); 
+//     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//     $output = curl_exec($ch);
+//     curl_close($ch);
+//     return json_decode($output, true);
+// }
 ?>
+<?php
+    //http://www.111cn.net/callback.php
+      
+    $appid = "wxdf937732890770c1";  
+    $secret = "d4624c36b6795d1d99dcf0547af5443d";  
+    $code = $_GET["code"];  
+    $get_token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$appid.'&secret='.$secret.'&code='.$code.'&grant_type=authorization_code';
+
+    $ch = curl_init();  
+    curl_setopt($ch,CURLOPT_URL,$get_token_url);  
+    curl_setopt($ch,CURLOPT_HEADER,0);  
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );  
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);  
+    $res = curl_exec($ch);  
+    curl_close($ch);  
+    $json_obj = json_decode($res,true);  
+      
+    //根据openid和access_token查询用户信息  
+    $access_token = $json_obj['access_token'];  
+    $openid = $json_obj['openid'];  
+    $get_user_info_url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';  
+      
+    $ch = curl_init();  
+    curl_setopt($ch,CURLOPT_URL,$get_user_info_url);  
+    curl_setopt($ch,CURLOPT_HEADER,0);  
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );  
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);  
+    $res = curl_exec($ch);  
+    curl_close($ch);  
+      
+    //解析json  
+    $user_obj = json_decode($res,true);  
+    $_SESSION['user'] = $user_obj;  
+    print_r($user_obj);  
+      
+    ?>  
--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php 
 //scope=snsapi_userinfo实例
-$appid='wxdf937732890770c1';
-$redirect_uri = urlencode ( 'http://zss27149.applinzi.com/getUserInfo.php' );
-$url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
-header("Location:".$url);
+// $appid='wxdf937732890770c1';
+// $redirect_uri = urlencode ( 'http://zss27149.applinzi.com/getUserInfo.php' );
+// $url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
+// header("Location:".$url);
 
 ?>
+<?php  
+      
+    if(isset($_SESSION['user'])){  
+        print_r($_SESSION['user']);
+    exit;
+    }
+    $APPID='wxdf937732890770c1';
+    $REDIRECT_URI='http://zss27149.applinzi.com/getUserInfo.php';
+    $scope='snsapi_base';
+    //$scope='snsapi_userinfo';//需要授权
+    $url='https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$APPID.'&redirect_uri='.urlencode($REDIRECT_URI).'&response_type=code&scope='.$scope.'&state='.$state.'#wechat_redirect';
+    
+    header("Location:".$url);
+    ?>
