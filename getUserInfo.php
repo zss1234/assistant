73d66c28af76890d9b9b878736d2655a7d68ba04
zhaoshasha <zss_27149@aliyun.com>
2016-10-21 16:31:39 +0800

    print nickname and headimgurl

--- getUserInfo.php
+++ getUserInfo.php
@@ -31,6 +31,9 @@
     //解析json  
     $user_obj = json_decode($res,true);  
     $_SESSION['user'] = $user_obj;  
-    print_r($user_obj);  
+    // print_r($user_obj);
+    $print_r($user_obj[nickname]);
+    $print_r($user_obj[headimgurl]);
+
       
     ?>  
