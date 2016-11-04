79a007f273447d936ab3f73a1dc683e5a475ff5c
zhaoshasha <zss_27149@aliyun.com>
2016-11-04 15:35:02 +0800

    20161104

--- content.php
+++ content.php
@@ -55,10 +55,13 @@ function conver2gbk($string){
         $res1[$num+1][ddsl] = array_sum($skfs_num);
 
 // 时段汇总
+    $res2[] = array();
+    $num2 = 0;
     $time_num = 0;
-    $sum_time = 0;//其实最主要的是上面的笔数 因为总金额就是当日的营业总金额营业总额根据不同查询方式是不会变化的
+    $sum_time = 0;
     for ($i=9; $i < 22; $i++) { 
-        // echo $i."&nbsp;&nbsp;";
+        $num2++;
+        $res2[$num2][sd] = $i;
         $time_sql = "select ysje from ftfx where SUBSTRING(jzdt, 1, 11) = '".$date."' and SUBSTRING(jzdt, 12, 2) = '".$i."' and SubStore = '".$dp_name."'";
         $time_exec = odbc_exec($conn, $time_sql);
         $time_arr = array();
@@ -67,17 +70,105 @@ function conver2gbk($string){
             $time_arr[$time] = $time_row[ysje];
             $time++;
         }
-        // echo array_sum($time_arr)."&nbsp;&nbsp;";//该时间段的销售总金额
-        // echo count($time_arr);//该时间段的销售总笔数
-        // echo "<br>";
+        $res2[$num2][sdsl] = count($time_arr);
+        $res2[$num2][sdje] = array_sum($time_arr);
         $time_num+=count($time_arr);
         $sum_time+=array_sum($time_arr);
     }
-    // echo $time_num;//总笔数
-    // echo $sum_time;//总金额
-        $res[] = array();
-        $res[0] = $res1;
-        echo json_encode($res);
+    $res2[$num2+1][sd] = "汇总";
+    $res2[$num2+1][sdsl] = $time_num;
+    $res2[$num2+1][sdje] = $sum_time;
+// 5.菜类汇总
+// SELECT lbname FROM jcfx WHERE SUBSTRING(jzrq, 1, 11) = '2016-09-26' AND SubStore = '测试' GROUP BY lbname
 
-    exit;
+    $category_sql = "SELECT lbname FROM jcfx WHERE SUBSTRING(jzrq, 1, 11) = '".$date."' AND SubStore = '".$dp_name."' GROUP BY lbname";
+    $category_exec = odbc_exec($conn, $category_sql);
+    $category_arr = array();
+    $category = 0;
+    $category_num = 0;
+    $num3 = 0;
+    $res3[] = array();
+    while ($category_row = odbc_fetch_array($category_exec)) {
+        $num3++;
+        $res3[$num3][clmc] = conver2utf8($category_row[lbname]);
+
+        // echo conver2utf8($category_row[lbname])."<br>";//菜类名称
+        $cate_sql = "SELECT * FROM jcfx WHERE SUBSTRING(jzrq, 1, 11) = '".$date."' AND SubStore = '".$dp_name."' AND lbname = '".$category_row[lbname]."'";
+        $cate_exec = odbc_exec($conn, $cate_sql);
+        $cate_arr = array();
+        $cate = 0;
+        while ($cate_row = odbc_fetch_array($cate_exec)) {
+            $cate_arr[$cate] += $cate_row[xsje];
+            $cate++;
+        }
+        // echo array_sum($cate_arr)."&nbsp;&nbsp;&nbsp;";//该菜类总销售金额
+        // echo count($cate_arr);//该菜类总销售数量
+        // echo "<br>";
+        $res3[$num3][sl] = count($cate_arr);
+        $res3[$num3][je] = array_sum($cate_arr);
+        $category_num += count($cate_arr);
+        $category_sum += array_sum($cate_arr);
+    }
+    $res3[$num3+1][clmc] = "汇总";
+    $res3[$num3+1][sl] = $category_num;
+    $res3[$num3+1][je] = $category_sum;
+
+// 6.菜品汇总
+    $cpname_sql = "SELECT jcname FROM jcfx WHERE SUBSTRING(jzrq, 1, 11) = '".$date."' AND SubStore = '".$dp_name."' GROUP BY jcname";
+    $cpname_exec = odbc_exec($conn, $cpname_sql);
+    $cpname_arr = array();
+    $cpname = 0;
+    $cpname_num = 0;
+    $num4 = 0;
+    $res4[] = array();
+    while ($cpname_row = odbc_fetch_array($cpname_exec)) {
+        $num4++;
+        $res4[$num4][cpmc] = conver2utf8($cpname_row[jcname]);
+        // echo conver2utf8($cpname_row[jcname])."<br>";//菜品名称
+        $cp_sql = "SELECT * FROM jcfx WHERE SUBSTRING(jzrq,1,11)= '".$date."' AND SubStore = '".$dp_name."' AND jcname = '".$cpname_row[jcname]."'";
+        $cp_exec = odbc_exec($conn,$cp_sql);
+        $cp_arr = array();
+        $cp = 0;
+        while ($cp_row = odbc_fetch_array($cp_exec)) {
+            $cp_arr[$cp] += $cp_row[xsje];
+            $cp++;
+        }
+        $res4[$num4][cpsl] = count($cp_arr);
+        $res4[$num4][cpje] = array_sum($cp_arr);
+        // echo array_sum($cp_arr)."&nbsp;&nbsp;&nbsp;";//该菜类总销售金额
+        // echo count($cp_arr);//该菜类总销售数量
+        // echo "<br>";
+        $cpname_num += count($cp_arr);
+        $cpname_sum += array_sum($cp_arr);
+    }
+    // echo $cpname_num;//总共销售菜类数量
+    $res4[$num4+1][cpmc] = "汇总";
+    $res4[$num4+1][cpsl] = $cpname_num;
+    $res4[$num4+1][cpje] = $cpname_sum;
+
+// $sql = "select jzdt from ftfx where SUBSTRING(jzdt, 1, 11) = '2016-09-26' and SubStore = '测试'";
+    $ftfx_sql = "select SubStore,ysje,krrs,jzdt from ftfx where SUBSTRING(jzdt, 1, 11) = '".$date."' and SubStore = '".$dp_name."'";
+    $ftfx_exec = odbc_exec($conn, $ftfx_sql);
+    $ftfx_sum = 0;
+    $ftfx_num = odbc_num_rows($ftfx_exec);//根据记录数来查询账单笔数，一个账单对应一条信息
+    $res5[] = array();
+    $num5 = 0;
+    while (@$ftfx_row = odbc_fetch_array($ftfx_exec)) {
+        $ftfx_sum += $ftfx_row[ysje];
+    }
+        $res5[$num5][sum] = $ftfx_sum;
+        $res5[$num5][num] = $ftfx_num;
+        $res5[$num5][round] = round($ftfx_sum/$ftfx_num);
+    // echo $ftfx_sum."<br>";//总的销售额
+    // echo $ftfx_num."<br>";//账单笔数
+    // echo round($ftfx_sum/$ftfx_num);//单均
+
+$res[] = array();
+$res[0] = $res1;
+$res[1] = $res2;
+$res[2] = $res3;
+$res[3] = $res4;
+$res[4] = $res5;
+echo json_encode($res);
+exit;
 ?>
--- indexs.php
+++ indexs.php
@@ -10,6 +10,7 @@
     $time = time();
     $date =  date("Y-m-d",$time);
     $date = "2016-09-26";
+
     // 获取页面上的用户应该是在session中保存的
 
     // 获取店铺名称，先判断该SRQuery数据库的SubStore表中name字段中是否存在该店铺名称
@@ -40,123 +41,6 @@ function conver2gbk($string){
     // echo odbc_num_rows($name_exec);//输出记录数
     // $row = odbc_fetch_array($name_exec);
     // echo conver2utf8($row['Name']);//输出店铺的名称
-?>
-
-<?php
-    // 2.根据房台分析分析表查询账单信息
-    include_once("connect.php");
-    // $sql = "select jzdt from ftfx where SUBSTRING(jzdt, 1, 11) = '2016-09-26' and SubStore = '测试'";
-    $ftfx_sql = "select SubStore,ysje,krrs,jzdt from ftfx where SUBSTRING(jzdt, 1, 11) = '".$date."' and SubStore = '".$dp_name."'";
-    $ftfx_exec = odbc_exec($conn, $ftfx_sql);
-    $ftfx_sum = 0;
-    $ftfx_num = odbc_num_rows($ftfx_exec);//根据记录数来查询账单笔数，一个账单对应一条信息
-    while (@$ftfx_row = odbc_fetch_array($ftfx_exec)) {
-        $ftfx_sum += $ftfx_row[ysje];
-    }
-    // echo $ftfx_sum."<br>";//总的销售额
-    // echo $ftfx_num."<br>";//账单笔数
-    // echo round($ftfx_sum/$ftfx_num);//单均
-?>
-<?php 
-    // 3.收款方式
-    $skfs_sql = "select skfs from skjl where jzrq = '".$date."' and SubStore = '".$dp_name."' group by skfs";
-    $skfs_exec = odbc_exec($conn, $skfs_sql);
-    $skfs_sum = array();
-    $skfs_num = array();
-    $num = 0;
-    while (@$skfs_row = odbc_fetch_array($skfs_exec)) {
-        $num++;
-        // echo  conver2utf8($skfs_row[skfs])."&nbsp;";//输出收款方式
-        $skjl_sql = "select je from skjl where jzrq = '".$date."' and skfs = '".$skfs_row[skfs]."'";
-        $skjl_exec = odbc_exec($conn, $skjl_sql);
-        $skjl_sum = 0;
-        $skjl_num = 0;
-        while (@$skjl_row = odbc_fetch_array($skjl_exec)) {
-            $skjl_sum += $skjl_row[je];
-            $skjl_num++;
-        }
-        // echo $skfs_sum[$num] = $skjl_sum."&nbsp;";//支付方式分类金额
-        // echo $skfs_num[$num] = $skjl_num."<br>";//支付方式分类订单数量
-    }
-    // echo array_sum($skfs_sum)."&nbsp;&nbsp;&nbsp;";//支付方式总金额
-    // echo array_sum($skfs_num);//支付方式分类总订单数量
-?>
-<?php 
-    // 4.时段汇总
-    $time_num = 0;
-    $sum_time = 0;//其实最主要的是上面的笔数 因为总金额就是当日的营业总金额营业总额根据不同查询方式是不会变化的
-    for ($i=9; $i < 22; $i++) { 
-        // echo $i."&nbsp;&nbsp;";
-        $time_sql = "select ysje from ftfx where SUBSTRING(jzdt, 1, 11) = '".$date."' and SUBSTRING(jzdt, 12, 2) = '".$i."' and SubStore = '".$dp_name."'";
-        $time_exec = odbc_exec($conn, $time_sql);
-        $time_arr = array();
-        $time = 0; 
-        while ($time_row = odbc_fetch_array($time_exec)) {
-            $time_arr[$time] = $time_row[ysje];
-            $time++;
-        }
-        // echo array_sum($time_arr)."&nbsp;&nbsp;";//该时间段的销售总金额
-        // echo count($time_arr);//该时间段的销售总笔数
-        // echo "<br>";
-        $time_num+=count($time_arr);
-        $sum_time+=array_sum($time_arr);
-    }
-    // echo $time_num;//总笔数
-    // echo $sum_time;//总金额
-?>
-<?php
-// 5.菜类汇总
-// SELECT lbname FROM jcfx WHERE SUBSTRING(jzrq, 1, 11) = '2016-09-26' AND SubStore = '测试' GROUP BY lbname
-
-    $category_sql = "SELECT lbname FROM jcfx WHERE SUBSTRING(jzrq, 1, 11) = '".$date."' AND SubStore = '".$dp_name."' GROUP BY lbname";
-    $category_exec = odbc_exec($conn, $category_sql);
-    $category_arr = array();
-    $category = 0;
-    $category_num = 0;
-    while ($category_row = odbc_fetch_array($category_exec)) {
-
-        // echo conver2utf8($category_row[lbname])."<br>";//菜类名称
-
-        $cate_sql = "SELECT * FROM jcfx WHERE SUBSTRING(jzrq, 1, 11) = '".$date."' AND SubStore = '".$dp_name."' AND lbname = '".$category_row[lbname]."'";
-        $cate_exec = odbc_exec($conn, $cate_sql);
-        $cate_arr = array();
-        $cate = 0;
-        while ($cate_row = odbc_fetch_array($cate_exec)) {
-            $cate_arr[$cate] += $cate_row[xsje];
-            $cate++;
-        }
-        // echo array_sum($cate_arr)."&nbsp;&nbsp;&nbsp;";//该菜类总销售金额
-        // echo count($cate_arr);//该菜类总销售数量
-        // echo "<br>";
-        $category_num += count($cate_arr);
-    }
-    // echo $category_num;//总共销售菜类数量
-?>
-<?php 
-    // 6.菜品汇总
-    $cpname_sql = "SELECT jcname FROM jcfx WHERE SUBSTRING(jzrq, 1, 11) = '".$date."' AND SubStore = '".$dp_name."' GROUP BY jcname";
-    $cpname_exec = odbc_exec($conn, $cpname_sql);
-    $cpname_arr = array();
-    $cpname = 0;
-    $cpname_num = 0;
-    while ($cpname_row = odbc_fetch_array($cpname_exec)) {
-
-        // echo conver2utf8($cpname_row[jcname])."<br>";//菜品名称
-        $cp_sql = "SELECT * FROM jcfx WHERE SUBSTRING(jzrq,1,11)= '".$date."' AND SubStore = '".$dp_name."' AND jcname = '".$cpname_row[jcname]."'";
-        $cp_exec = odbc_exec($conn,$cp_sql);
-        $cp_arr = array();
-        $cp = 0;
-        while ($cp_row = odbc_fetch_array($cp_exec)) {
-            $cp_arr[$cp] += $cp_row[xsje];
-            $cp++;
-        }
-        // echo array_sum($cp_arr)."&nbsp;&nbsp;&nbsp;";//该菜类总销售金额
-        // echo count($cp_arr);//该菜类总销售数量
-        // echo "<br>";
-        $cpname_num += count($cp_arr);
-    }
-    // echo $cpname_num;//总共销售菜类数量
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -220,15 +104,15 @@ function conver2gbk($string){
 			<ul class="biaodan">
 			  <li style="border-right:1px solid #2E8906">
 				<p style="text-align: center;">营业额</p>
-				<p style="color:#000;font-size:22px;text-align: center;"><b><?php echo $ftfx_sum?></b></p>
+				<p style="color:#000;font-size:22px;text-align: center;"><b id="sum">0</b></p>
 			  </li>
 			  <li style="border-right:1px solid #2E8906">
 				<p style="text-align: center;">客单数</p>
-				<p style="color:#000;font-size:22px;text-align: center;"><b><?php echo $ftfx_num?></b></p>
+				<p style="color:#000;font-size:22px;text-align: center;"><b id="num">0</b></p>
 			  </li>
 			  <li>
 				<p style="text-align: center;">单均</p>
-				<p style="color:#000;font-size:22px;text-align: center;"><b><?php echo round($ftfx_sum/$ftfx_num)?></b></p>
+				<p style="color:#000;font-size:22px;text-align: center;"><b id="round">0</b></p>
 			  </li>
 			</ul>
 		  </div>
@@ -254,31 +138,44 @@ function conver2gbk($string){
                 <div class="tab-content" style="display:none">
                     <h3>时段汇总</h3>
                     <table class="payment">
-                        <tr>
-                            <td>时段汇总</td>
-                            <td>笔数</td>
-                            <td>金额</td>
-                        </tr>
+                        <thead>
+                            <tr>
+                                <td>时段汇总</td>
+                                <td>笔数</td>
+                                <td>金额</td>
+                            </tr>
+                        </thead>
+                        <tbody id="tabs2">
+
+                        </tbody>
+
+
                     </table>                    
                 </div>
                 <div class="tab-content" style="display:none">
                     <h3>菜类汇总</h3>
                     <table class="payment">
-                        <tr>
-                            <td>菜品类别</td>
-                            <td>数量</td>
-                            <td>金额</td>
-                        </tr>
+                        <thead>
+                            <tr>
+                                <td>菜品类别</td>
+                                <td>数量</td>
+                                <td>金额</td>
+                            </tr>
+                        </thead>
+                        <tbody id="tabs3"></tbody>
                     </table>                    
                 </div>
                 <div class="tab-content" style="display:none">
                     <h3>菜品汇总</h3>
                     <table class="payment">
-                        <tr>
-                            <td>菜品名称</td>
-                            <td>数量</td>
-                            <td>金额</td>
-                        </tr>
+                        <thead>
+                            <tr>
+                                <td>菜品名称</td>
+                                <td>数量</td>
+                                <td>金额</td>
+                            </tr>
+                        </thead>
+                        <tbody id="tabs4"></tbody>
                     </table>                    
                 </div>
             </div>
@@ -299,23 +196,47 @@ function conver2gbk($string){
          var str1="";
          var str2="";
          var str3="";
+         var str4="";
+         var st_sum = "";
+         var st_num = "";
+         var st_round = "";
+
         $.ajax({
                 type: "get",
                 dataType: "json",
                 url: "content.php",//地址
                 data: "q=" + date,
                 success: function(data){
-                    for(var x=0;x<data.length;x++){
-                        for(var y=1;y<data[x].length;y++){
-                            str1 += "<tr>";
-                            str1 += "<td>" + data[x][y].skfs +"</td>";
-                            str1 += "<td>" + data[x][y].ddsl +"</td>";
-                            str1 += "<td>" + data[x][y].flje +"</td>";
-                            str1 += "<tr>";  
-
-                        }
+                    var t1 = data[0].length;
+                    var t2 = data[1].length;
+                    var t3 = data[2].length;
+                    var t4 = data[3].length;
+                    
+                    for(var x=1;x<t1;x++){
+                        str1 += "<tr><td>" + data[0][x].skfs +"</td><td>" + data[0][x].ddsl +"</td><td>" + data[0][x].flje +"</td></tr>";
+                    }
+                    for(var x=1;x<t2;x++){
+                        str2 += "<tr><td>" + data[1][x].sd +"</td><td>" + data[1][x].sdsl +"</td><td>" + data[1][x].sdje +"</td></tr>";
+                    }
+                    for(var x=1;x<t3;x++){
+                        str3 += "<tr><td>" + data[2][x].clmc +"</td><td>" + data[2][x].sl +"</td><td>" + data[2][x].je +"</td></tr>";
+                    }
+                    for(var x=1;x<t4;x++){
+                        str4 += "<tr><td>"+data[3][x].cpmc+"</td><td>"+data[3][x].cpsl+"</td><td>"+data[3][x].cpje+"</td></tr>";
                     }
                     $("#tabs1").html(str1);
+                    $("#tabs2").html(str2);
+                    $("#tabs3").html(str3);
+                    $("#tabs4").html(str4);
+                    st_sum = data[4][0].sum;
+                    $("#sum").html(st_sum);
+                    st_num = data[4][0].num;
+                    $("#num").html(st_num);
+                    st_round = data[4][0].round;
+                    $("#round").html(st_round);
+
+
+
                 }
     });
     });
